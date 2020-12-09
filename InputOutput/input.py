68d5334ce263eb5892d0c1429f15c1d71c273bf6
Wonjin Lee <claude17@naver.com>
2020-12-09 21:15:07 +0900

    Create input.py

---
+++ InputOutput/input.py
@@ -0,0 +1,11 @@
+''' 공백을 기준으로 구분된 데이터를 입력 받을 때 '''
+li = list(map(int, input().split()))
+
+''' 공백을 기준으로 구분된 데이터를 입력 받을 때 (개수가 많지 않다면) '''
+a, b, c = map(int, input().split())
+
+''' 2차원 배열 데이터를 입력 받을 때 (N = 3, M = 4) '''
+n = 3
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
